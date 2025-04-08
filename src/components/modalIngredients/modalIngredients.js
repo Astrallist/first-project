d63d3eb8de7747f518fd222e6ed056b3c5984390
Astrallist <132389598+Astrallist@users.noreply.github.com>
2025-04-08 05:08:04 +0300

    + propTypes

--- src/components/modalIngredients/modalIngredients.js
+++ src/components/modalIngredients/modalIngredients.js
@@ -34,4 +34,15 @@ class ModalIngredients extends React.Component {
 	}
 }
 
-export default ModalIngredients;
+ModalIngredients.propTypes = {
+	content: PropTypes.shape({
+	  image_large: PropTypes.string.isRequired,
+	  name: PropTypes.string.isRequired,
+	  calories: PropTypes.number.isRequired,
+	  proteins: PropTypes.number.isRequired,
+	  fat: PropTypes.number.isRequired,
+	  carbohydrates: PropTypes.number.isRequired,
+	}).isRequired,
+  };
+
+  export default ModalIngredients;
