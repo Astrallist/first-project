4bcff7e188ed388944e95165fbc58d1de2a8215e
Astrallist <132389598+Astrallist@users.noreply.github.com>
2025-04-08 05:16:45 +0300

    API из redux

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -4,29 +4,18 @@ import AppHeader from '../appHeader/appHeader';
 import BurgerIngredients from '../burgerIngredients/burgerIngredients';
 import BurgerConstructor from '../burgerConstructor/burgerConstructor';
 
-const API = 'https://norma.nomoreparties.space/api';
+import { useDispatch } from 'react-redux';
+import { getIngredients } from '../../services/actions/ingredients';
+
+
 
 export const App = () => {
-	const [count, setCount] = useState(0);
-	const [ingredients, setIngredients] = useState([]);
 	const [error, setError] = useState(null);
-
-	const fetchIngredients = async () => {
-		try {
-			const response = await fetch(`${API}/ingredients`);
-			if (!response.ok) {
-				throw new Error(`Ошибка: ${response.status}`);
-			}
-			const data = await response.json();
-			setIngredients(data.data);
-		} catch (err: any) {
-			setError(err.message);
-		}
-	};
+	const dispatch = useDispatch<any>();
 
 	useEffect(() => {
-		fetchIngredients();
-	}, []);
+	  dispatch(getIngredients());
+	}, [dispatch]);
 
 	if (error) {
 		return <div>Ошибка: {error}</div>;
@@ -38,7 +27,7 @@ export const App = () => {
 		<>
 			<AppHeader />
 			<div className='content'>
-				<BurgerIngredients ingredients={ingredients} />
+				<BurgerIngredients/>
 				<BurgerConstructor />
 			</div>
 		</>
