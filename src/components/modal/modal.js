669c7555531b1acfc4de6c59d3182d7f7107cb70
Astrallist <132389598+Astrallist@users.noreply.github.com>
2025-04-08 05:10:35 +0300

    + ModalOverlay (компонент)

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Children } from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import styles from './modal.module.css';
@@ -8,6 +8,23 @@ import ModalIngredients from '../modalIngredients/modalIngredients';
 
 const modalRoot = document.getElementById("react-modals");
 
+const ModalOverlay = ({ children, onClose }) => {
+	const handleOverlayClick = (event) => {
+		if (event.target === event.currentTarget) {
+			onClose();
+		}
+	};
+	return (
+		< div className={styles.modalOverlay} onClick={handleOverlayClick} >
+			{children}
+		</div >
+	)
+}
+
+ModalOverlay.propTypes = {
+    children: PropTypes.node.isRequired,
+    onClose: PropTypes.func.isRequired
+};
 
 class Modal extends React.Component {
 
@@ -16,11 +33,7 @@ class Modal extends React.Component {
 			this.props.onClose();
 		}
 	};
-	handleOverlayClick = (event) => {
-		if (event.target === event.currentTarget) {
-			this.props.onClose();
-		}
-	};
+
 	componentDidMount() {
 		document.addEventListener('keydown', this.handleKeyDown);
 	}
@@ -34,7 +47,7 @@ class Modal extends React.Component {
 		return ReactDOM.createPortal(
 			(
 				<>
-					<div className={styles.modalOverlay} onClick={this.handleOverlayClick}>
+					<ModalOverlay onClose={onClose}>
 						<div className={styles.modalContent}>
 							<div className={styles.title}>
 								{type === 'ingredient' ? <h1 className='text text_type_main-large'>Детали ингридиента</h1> : <></>}
@@ -48,17 +61,21 @@ class Modal extends React.Component {
 								{type === 'order' ? <ModalOrder orderNumber={orderNumber}></ModalOrder> : <></>}
 							</div>
 						</div>
-					</div>
+					</ModalOverlay>
 				</>
 			),
 			document.getElementById("react-modals")
 		);
 
-
-
-
 	}
 }
+
+Modal.propTypes = {
+    type: PropTypes.oneOf(['ingredient', 'order']).isRequired,
+    content: PropTypes.object,
+    onClose: PropTypes.func.isRequired,
+    orderNumber: PropTypes.number
+};
 
 export default Modal;
 
